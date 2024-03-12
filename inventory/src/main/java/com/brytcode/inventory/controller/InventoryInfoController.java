981be24be4b33cadad6812ba5d7e2ca1896ff010
theomnicode <srikrishna.jvm@gmail.com>
2024-03-12 08:02:09 +0530

    API Gateway

--- inventory/src/main/java/com/brytcode/inventory/controller/InventoryInfoController.java
+++ inventory/src/main/java/com/brytcode/inventory/controller/InventoryInfoController.java
@@ -1,5 +1,9 @@
 package com.brytcode.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/inventory")
 public class InventoryInfoController {
+
+    @Autowired
+    private Environment environment;
+    int port;
     @GetMapping("info")
     public String getInfo(){
-        return "Welcome to Inventory Service";
+        return "Welcome to Inventory Service from " + environment.getProperty("local.server.port");
     }
 }
--- sales/src/main/java/com/brytcode/sales/controller/SalesInfoController.java
+++ sales/src/main/java/com/brytcode/sales/controller/SalesInfoController.java
@@ -1,14 +1,19 @@
 package com.brytcode.sales.controller;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/sales")
 public class SalesInfoController {
     @GetMapping("info")
     public String getInfo(){
-        return "Welcome to Sales Service";
+        RestTemplate inventory = new RestTemplate();
+        ResponseEntity<String> inventoryResponse = inventory.exchange("http://172.24.144.1:9090/inventory/inventory/info", HttpMethod.GET, null, String.class );
+        return "Welcome to Sales Service -> "+inventoryResponse.getBody();
     }
 }
